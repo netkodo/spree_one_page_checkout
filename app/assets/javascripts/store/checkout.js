51df569a4f34a4809538ee2e687510961c5ef314
szax <szax22@gmail.com>
2014-09-30 11:05:50 +0200

    - fix js

--- app/assets/javascripts/store/checkout.js
+++ app/assets/javascripts/store/checkout.js
@@ -38,20 +38,20 @@
       });
       update_state('b');
       update_state('s');
-      ($('input#order_use_billing')).click(function() {
-        if (($(this)).is(':checked')) {
-          ($('#shipping .inner')).hide();
-          return ($('#shipping .inner input, #shipping .inner select')).prop('disabled', true);
-        } else {
-          ($('#shipping .inner')).show();
-          ($('#shipping .inner input, #shipping .inner select')).prop('disabled', false);
-          if (get_states('s')) {
-            return ($('span#sstate input')).hide().prop('disabled', true);
-          } else {
-            return ($('span#sstate select')).hide().prop('disabled', true);
-          }
-        }
-      }).triggerHandler('click');
+//      ($('input#order_use_billing')).click(function() {
+//        if (($(this)).is(':checked')) {
+//          ($('#shipping .inner')).hide();
+//          return ($('#shipping .inner input, #shipping .inner select')).prop('disabled', true);
+//        } else {
+//          ($('#shipping .inner')).show();
+//          ($('#shipping .inner input, #shipping .inner select')).prop('disabled', false);
+//          if (get_states('s')) {
+//            return ($('span#sstate input')).hide().prop('disabled', true);
+//          } else {
+//            return ($('span#sstate select')).hide().prop('disabled', true);
+//          }
+//        }
+//      }).triggerHandler('click');
     }
     if (($('#checkout_form_payment')).is('*')) {
       ($('input[type="radio"][name="order[payments_attributes][][payment_method_id]"]')).click(function() {
